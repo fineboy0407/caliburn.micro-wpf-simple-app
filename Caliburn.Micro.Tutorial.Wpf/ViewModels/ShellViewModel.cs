da8920b35513d9c9c7a096b6694d6b3075b42000
fineboy0407 <levilee.fine@gmail.com>
2023-02-22 01:07:06 -0500

    feat: introduce caliburn.micro to wpf

---
+++ Caliburn.Micro.Tutorial.Wpf/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.Tutorial.Wpf.ViewModels
+{
+    public class ShellViewModel:Conductor<object>
+    {
+
+    }
+}
